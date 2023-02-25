e9134cc75081e31df37c033ce61cba2982cb7512
me <me@robertcurtiss.com>
2023-02-24 19:26:22 -0500

    Tutorial 7 Completed

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date_pulished")
+    pub_date = models.DateTimeField("Date Published")
 
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently")
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
